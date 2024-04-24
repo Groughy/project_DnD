26d5d9d2fb75045eb596a8d16a718e6c02fd685a
Sébastien Journiac <152491383+Groughy@users.noreply.github.com>
2024-04-24 15:54:35 +0200

    On crée des armes Et l'arme de base qui tape à 0, la nulle.

--- src/Character.java
+++ src/Character.java
@@ -1,13 +1,42 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Character {
-String name;
-CharacterClasse getClasse;
+    private String name;
+    private String characterClass;
+    private int lifePoints;
+    private int attackPoints;
+    private Weapon weapon;
 
-public Character(String name,CharacterClasse classe) {
-    this.name = name;
-   getClasse = classe;
-}
+    private Character character(String name,  String characterClass) {
+        this.name = name;
+        this.characterClass = characterClass;
+        if(characterClass.equals("Warrior")){
+            this.lifePoints = 10;
+            this.attackPoints = 10;
+            this.weapon = new Weapon("beginningSword",0, "Warrior");
+        }
+        else {
+            this.characterClass = "Mage";
+            this.lifePoints = 6;
+            this.attackPoints = 15;
+            this.weapon = new Weapon("beginningSpell", 0, "Mage");
+        }
+        return this;
+    }
     public static void main(String[] args) {
-        Character firstCharacter = new Character("Truc", Warrior);
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the name of your character: ");
+        String name = scanner.nextLine();
+        System.out.println("Enter the class of your character: ");
+        String getClass = scanner.nextLine();
+       Character someone = new Character().character(name, getClass);
+        System.out.println(someone.name + " is a " + someone.characterClass + " with " + someone.lifePoints + " life points, " + someone.attackPoints + " attack points, and is equipped with a " + someone.weapon.name + ".");
+    }
+    public void setWeapon(Weapon weapon) {
+        if (!weapon.getClassRestriction().equals(this.characterClass)) {
+            throw new IllegalArgumentException("This weapon is not allowed for this class.");
+        }
+        this.weapon = weapon;
     }
 }
---
+++ src/Weapon.java
@@ -0,0 +1,31 @@
+public class Weapon {
+    String name;
+    private int damage;
+    private String classRestriction;
+
+    public Weapon(String name, int damage, String classRestriction) {
+        this.name = name;
+        this.damage = damage;
+        this.classRestriction = classRestriction;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public String getClassRestriction() {
+        return classRestriction;
+    }
+
+    public static void main(String[] args) {
+        Weapon[] equipments = {
+                new Weapon("Sword", 5, "Warrior"),
+                new Weapon("Mace", 3, "Warrior"),
+                new Weapon("Fire Ball", 7, "Mage"),
+                new Weapon("Thunder Bolt", 2, "Mage")
+        };
+
+        for (Weapon equipment : equipments) {
+            System.out.println(equipment.name + " has a damage of " + equipment.damage + ".");
+        }
+    }
+}
