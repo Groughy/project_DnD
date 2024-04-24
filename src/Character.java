508f4e061a11548f5be8471e7b4bae550ca7ad3b
Sébastien Journiac <152491383+Groughy@users.noreply.github.com>
2024-04-24 11:31:40 +0200

    On test des classes

---
+++ src/Character.java
@@ -0,0 +1,13 @@
+public class Character {
+String name;
+CharacterClasse getClasse;
+
+public Character(String name,CharacterClasse classe) {
+    this.name = name;
+   getClasse = classe;
+}
+    public static void main(String[] args) {
+        Character firstCharacter = new Character("Truc", Warrior);
+
+    }
+}
