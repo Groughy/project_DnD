7ad8c11df83b9b3b50f39d9df733abde26c34a8b
Sébastien Journiac <152491383+Groughy@users.noreply.github.com>
2024-04-25 16:07:59 +0200

    Ouais ouais ouais Les petites retouches

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Welcome to the game! (•̀ᴗ•́)و ̑̑");
+        System.out.println("Wanna play ? (yes/no) (੭ ᵔ³ᵔ)੭ 口");
+        if (new Scanner(System.in).nextLine().equals("yes")) {
+            Menu game = new Menu();
+            game.play();
+        }
+        else {
+            System.out.println("Go to hell ! █▬▬ ◟(`ﮧ´ ◟ )");
+        }
     }
 }
--- src/Menu.java
+++ src/Menu.java
@@ -6,18 +6,7 @@ public class Menu {
     private Random rand = new Random();
     private int rollDice(){
         return rand.nextInt(5)+1;}
-    public static void main(String[] args) {
-        System.out.println("Welcome to the game! (•̀ᴗ•́)و ̑̑");
-        System.out.println("Wanna play ? (yes/no) (੭ ᵔ³ᵔ)੭ 口");
-        if (new Scanner(System.in).nextLine().equals("yes")) {
-            Menu game = new Menu();
-            game.play();
-        }
-        else {
-            System.out.println("Go to hell ! █▬▬ ◟(`ﮧ´ ◟ )");
-        }
-    }
-    private void play() {
+    void play() {
         Character.main(null);
         int i = 0;
         while (i < board.length) {
